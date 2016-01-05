a22762906bf8d21d3f78cb971ed720ef76151d1a
Duc Nghiem Xuan <xuanduc987@gmail.com>
2016-01-05 08:23:38 +0700

    Config rspec as unit test
    Summary: Config `rspec` as unit test engine for arcanist
    
    Test Plan:
    Run `arc unit`
    It should show `rspec` results.
    
    Reviewers: xuanduc987
    
    Reviewed By: xuanduc987
    
    Maniphest Tasks: T3
    
    Differential Revision: http://sources.xsoftvn.com/D3

---
+++ .arcanist-extensions/rspec_test_engine/src/RSpecTestEngine.php
@@ -0,0 +1,59 @@
+<?php
+
+final class RSpecTestEngine extends ArcanistUnitTestEngine {
+
+  public function run() {
+    $output = new TempFile();
+
+    $command = $this->getConfigurationManager()->getConfigFromAnySource('unit.engine.rspec.command');
+    if (!$command) $command = 'rspec';
+
+    $future = new ExecFuture($command .' -f json -o '. $output .' -f progress');
+
+    do {
+      list($stdout, $stderr) = $future->read();
+      echo $stdout;
+      sleep(0.5);
+    } while (!$future->isReady());
+
+    return $this->parseOutput(Filesystem::readFile($output));
+  }
+
+  public function shouldEchoTestResults() {
+    return true;
+  }
+
+  private function parseOutput($output) {
+    $results = array();
+
+    $json = json_decode($output, true);
+
+    foreach ($json['examples'] as $example) {
+      $result = new ArcanistUnitTestResult();
+      $result->setName($example['full_description']);
+
+      if (array_key_exists('run_time', $example)) {
+        $result->setDuration($example['run_time']);
+      }
+
+      switch ($example['status']) {
+        case 'passed':
+          $result->setResult(ArcanistUnitTestResult::RESULT_PASS);
+          break;
+
+        case 'failed':
+          $result->setResult(ArcanistUnitTestResult::RESULT_FAIL);
+          $result->setUserData($example['exception']['message']);
+          break;
+
+        case 'pending':
+          $result->setResult(ArcanistUnitTestResult::RESULT_SKIP);
+          break;
+      }
+
+      $results[] = $result;
+    }
+
+    return $results;
+  }
+}
